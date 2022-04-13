70d23a6bfd89b34d087b75d956eb353732b428f1
Shantanu Shukla <shantanushukla64@gmail.com>
2022-04-13 12:29:12 +0530

    resolved icon issue and loading code

--- frontend/src/reducers/filenameSelection.js
+++ frontend/src/reducers/filenameSelection.js
@@ -3,16 +3,7 @@ export const DSAFiles = [
         "type": "file",
         "name": "main.cpp",
         "id": "sa1GQ3eYSvOv1kRvhEoQV",
-        "content": 
-`#include<stdio.h>
-
-int main() {
-   int x=10;
-   int y=25;
-   int z=x+y;
-   printf("Sum of x+y = %i", z);
-}
-`
+        "content": ``
 
     },
     {
