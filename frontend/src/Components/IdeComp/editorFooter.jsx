55138f499afa14864f295e29c10ab0e564da9564
Shantanu Shukla <shantanushukla64@gmail.com>
2022-10-22 04:36:23 +0530

    added prompt for changing file name

--- frontend/src/Components/IdeComp/editorFooter.jsx
+++ frontend/src/Components/IdeComp/editorFooter.jsx
@@ -1,212 +1,224 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeFileName, savefile} from "../../actions";
-import { motion } from "framer-motion"
+import { changeFileName, savefile } from "../../actions";
+import { motion } from "framer-motion";
 import { changeLang } from "../../actions";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import "./editorFooter.css";
 const env = process.env.NODE_ENV; // current environment
-let url
-if(env === "development") {
-    url = 'http://localhost:5000/'
-}else{
-  url = 'https://lamtellbackend.herokuapp.com/'
+let url;
+if (env === "development") {
+  url = "http://localhost:5000/";
+} else {
+  url = "https://lamtellbackend.herokuapp.com/";
 }
 
 //let closeLangOption = false
 function Footer(props) {
-  const [fileName, setFileName] = useState('main.c');
+  const [fileName, setFileName] = useState("main");
   const dispatch = useDispatch();
   const inout = useSelector((state) => state.inout);
   const editorLang = useSelector((state) => state.editorLang);
-  const [ext, setExtension] = useState(".c")
-  const userId = localStorage.getItem("userId")
+  const [ext, setExtension] = useState(".c");
+  const userId = localStorage.getItem("userId");
   const [Ln, setLn] = useState(1);
   const [Col, setCol] = useState(1);
   const file = useSelector((state) => state.file);
   const userName = useSelector((state) => state.user);
   const languageList = {
-    "C": "c",
+    C: "c",
     "C-99": "c99",
     "C++": "cpp",
     "C++ 14": "cpp14",
     "C++ 17": "cpp17",
-    "PHP": "php",
-    "Perl": "perl",
+    PHP: "php",
+    Perl: "perl",
     "Python 2": "python2",
     "Python 3": "python3",
-    "Ruby": "ruby",
+    Ruby: "ruby",
     "GO Lang": "go",
-    "Scala": "scala",
+    Scala: "scala",
     "Bash Shell": "bash",
-    "SQL": "sql",
-    "Pascal": "pascal",
+    SQL: "sql",
+    Pascal: "pascal",
     "C#": "csharp",
     "VB.Net": "vbn",
-    "Haskell": "haskell",
+    Haskell: "haskell",
     "Objective C": "objc",
-    "Swift": "swift",
-    "Groovy": "groovy",
-    "Fortran": "fortran",
-    "Lua": "lua",
-    "TCL": "tcl",
-    "Hack": "hack",
-    "RUST": "rust",
-    "D": "d",
-    "Ada": "ada",
-    "Java": "java",
+    Swift: "swift",
+    Groovy: "groovy",
+    Fortran: "fortran",
+    Lua: "lua",
+    TCL: "tcl",
+    Hack: "hack",
+    RUST: "rust",
+    D: "d",
+    Ada: "ada",
+    Java: "java",
     "R Language": "r",
     "FREE BASIC": "freebasic",
-    "VERILOG": "verilog",
-    "COBOL": "cobol",
-    "Dart": "dart",
-    "YaBasic": "yabasic",
-    "Clojure": "clojure",
-    "NodeJS": "nodejs",
-    "Scheme": "scheme",
-    "Forth": "forth",
-    "Prolog": "prolog",
-    "Octave": "octave",
-    "CoffeeScript": "coffeescript",
-    "Icon": "icon",
+    VERILOG: "verilog",
+    COBOL: "cobol",
+    Dart: "dart",
+    YaBasic: "yabasic",
+    Clojure: "clojure",
+    NodeJS: "nodejs",
+    Scheme: "scheme",
+    Forth: "forth",
+    Prolog: "prolog",
+    Octave: "octave",
+    CoffeeScript: "coffeescript",
+    Icon: "icon",
     "F#": "fsharp",
     "Assembler - NASM": "nasm",
     "Assembler - GCC": "gccasm",
-    "Intercal": "intercal",
-    "Nemerle": "nemerle",
-    "Ocaml": "ocaml",
-    "Unlambda": "unlambda",
-    "Picolisp": "picolisp",
-    "SpiderMonkey": "spidermonkey",
+    Intercal: "intercal",
+    Nemerle: "nemerle",
+    Ocaml: "ocaml",
+    Unlambda: "unlambda",
+    Picolisp: "picolisp",
+    SpiderMonkey: "spidermonkey",
     "Rhino JS": "rhino",
-    "CLISP": "clisp",
-    "Elixir": "elixir",
-    "Factor": "factor",
-    "Falcon": "falcon",
-    "Fantom": "fantom",
-    "Nim": "nim",
-    "Pike": "pike",
-    "SmallTalk": "smalltalk",
+    CLISP: "clisp",
+    Elixir: "elixir",
+    Factor: "factor",
+    Falcon: "falcon",
+    Fantom: "fantom",
+    Nim: "nim",
+    Pike: "pike",
+    SmallTalk: "smalltalk",
     "OZ Mozart": "mozart",
-    "LOLCODE": "lolcode",
-    "Racket": "racket",
-    "Kotlin": "kotlin",
-    "Whitespace": "whitespace",
-    "Erlang": "erlang",
-    "J": "jlang",
-    "Brainf**k":"brainfuck"
+    LOLCODE: "lolcode",
+    Racket: "racket",
+    Kotlin: "kotlin",
+    Whitespace: "whitespace",
+    Erlang: "erlang",
+    J: "jlang",
+    "Brainf**k": "brainfuck",
   };
   const extension = {
-    "c": ".c",
-    "c99": ".c",
-    "cpp": ".cpp",
-    "cpp14": ".cpp",
-    "cpp17": ".cpp",
-    "php": ".php",
-    "perl": ".perl",
-    "python2": ".py",
-    "python3": ".py",
-    "ruby": ".ruby",
-    "go": ".go",
-    "scala": ".scala",
-    "bash": ".bash",
-    "sql": ".sql",
-    "pascal": ".pas",
-    "csharp": ".csx",
-    "vbn": ".vb",
-    "haskell": ".ha",
-    "objc": ".m",
-    "swift": ".swift",
-    "groovy": ".groovy",
-    "fortran": ".f95",
-    "lua": ".lua",
-    "tcl": ".tcl",
-    "hack": ".hack",
-    "rust": ".rs",
-    "d": ".d",
-    "ada": ".adb",
-    "java": ".java",
-    "r": ".r",
-    "freebasic": ".bas",
-    "verolig": ".v",
-    "cobol": ".cobc",
-    "dart": ".dart",
-    "yabasic": ".bas",
-    "clojure": ".clj",
-    "nodejs": ".js",
-    "scheme": ".sc",
-    "forth": ".fth",
-    "prolog": ".pg",
-    "octave": ".m",
-    "coffeescript": ".coffee",
-    "icon": ".icn",
-    "fsharp": ".fs",
-    "nasm": ".asm",
-    "gccasm": ".s",
-    "intercal": ".i",
-    "nemerle": ".n",
-    "ocaml": ".ml",
-    "unlambda": ".un",
-    "picolisp": ".pil",
-    "spidermonkey": ".js",
-    "rhino": ".js",
-    "clisp": ".lisp",
-    "elixir": ".exs",
-    "factor": ".factor",
-    "falcon": ".fl",
-    "fantom": ".fan",
-    "nim": ".nim",
-    "pike": ".pike",
-    "smalltalk": ".gst",
-    "mozart": ".oza",
-    "lolcode": ".lo",
-    "racket": ".rkt",
-    "kotlin": ".kt",
-    "whitespace": ".ws",
-    "erlang": ".erl",
-    "jlang": ".jl",
-    "brainfuck":".bf"
-  }
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setFileName(event.target.value);
-    dispatch(changeFileName(event.target.value));
+    c: ".c",
+    c99: ".c",
+    cpp: ".cpp",
+    cpp14: ".cpp",
+    cpp17: ".cpp",
+    php: ".php",
+    perl: ".perl",
+    python2: ".py",
+    python3: ".py",
+    ruby: ".ruby",
+    go: ".go",
+    scala: ".scala",
+    bash: ".bash",
+    sql: ".sql",
+    pascal: ".pas",
+    csharp: ".csx",
+    vbn: ".vb",
+    haskell: ".ha",
+    objc: ".m",
+    swift: ".swift",
+    groovy: ".groovy",
+    fortran: ".f95",
+    lua: ".lua",
+    tcl: ".tcl",
+    hack: ".hack",
+    rust: ".rs",
+    d: ".d",
+    ada: ".adb",
+    java: ".java",
+    r: ".r",
+    freebasic: ".bas",
+    verolig: ".v",
+    cobol: ".cobc",
+    dart: ".dart",
+    yabasic: ".bas",
+    clojure: ".clj",
+    nodejs: ".js",
+    scheme: ".sc",
+    forth: ".fth",
+    prolog: ".pg",
+    octave: ".m",
+    coffeescript: ".coffee",
+    icon: ".icn",
+    fsharp: ".fs",
+    nasm: ".asm",
+    gccasm: ".s",
+    intercal: ".i",
+    nemerle: ".n",
+    ocaml: ".ml",
+    unlambda: ".un",
+    picolisp: ".pil",
+    spidermonkey: ".js",
+    rhino: ".js",
+    clisp: ".lisp",
+    elixir: ".exs",
+    factor: ".factor",
+    falcon: ".fl",
+    fantom: ".fan",
+    nim: ".nim",
+    pike: ".pike",
+    smalltalk: ".gst",
+    mozart: ".oza",
+    lolcode: ".lo",
+    racket: ".rkt",
+    kotlin: ".kt",
+    whitespace: ".ws",
+    erlang: ".erl",
+    jlang: ".jl",
+    brainfuck: ".bf",
+  };
+  // const handleNameChange = (event) => {
+  //   let newFileName = prompt("Enter file name: ");
+  //   console.log(newFileName)
+  //   console.log(newFileName)
+  //   setFileName(newFileName);
+  //   dispatch(changeFileName(newFileName));
+  // };
+  const ChangeFileName = () => {
+    let newFileName = prompt("Enter file name: ");
+    if (newFileName) {
+      setFileName(newFileName);
+      dispatch(changeFileName(newFileName));
+    }
   };
 
   const handleFileSave = () => {
-    dispatch(savefile(file, editorLang,userId));
+    dispatch(savefile(file, editorLang, userId));
   };
   const changeLangFunc = (value) => {
-    setExtension(extension[`${value}`])
-    setFileName(() => 'main'+ extension[`${value}`]);
-    dispatch(changeLang(value))
-  }
-  const handleShareFile = (e) => {
-    axios.post(url+'share',{
-      code:file.content,
-      input:inout[0].content,
-      output:inout[1].content
-    }).then(res => {
-      navigator.clipboard.writeText(window.location.href + res.data.response._id);
-      alert("Copied the text");
-    })
+    setExtension(extension[`${value}`]);
+    setFileName(() => fileName);
+    dispatch(changeLang(value));
   };
-
+  const handleShareFile = (e) => {
+    axios
+      .post(url + "share", {
+        code: file.content,
+        input: inout[0].content,
+        output: inout[1].content,
+      })
+      .then((res) => {
+        navigator.clipboard.writeText(
+          window.location.href + res.data.response._id
+        );
+        alert("Copied the text");
+      });
+  };
 
   useEffect(() => {
     let isCtrl = false,
-    langOptions = false;
+      langOptions = false;
     document.documentElement.addEventListener("click", (e) => {
-      if(e.target.className !== 'footer_text l_footer change_lang'){
+      if (e.target.className !== "footer_text l_footer change_lang") {
         if (langOptions === true) {
-          console.log("WORKING")
+          console.log("WORKING");
 
           langOptions = false;
         }
       }
-    })
+    });
     document
       .getElementsByClassName("change_lang")[0]
       .addEventListener("click", () => {
@@ -216,13 +228,13 @@ function Footer(props) {
           langOptions = false;
         }
       });
-    document.addEventListener('keydown', (e) => {
-      if (e.key === 's' && e.ctrlKey) {
-          e.preventDefault();
-          dispatch(savefile(file,editorLang, userId));
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "s" && e.ctrlKey) {
+        e.preventDefault();
+        dispatch(savefile(file, editorLang, userId));
       }
     });
-  
+
     const codeText = document.getElementsByClassName("codeText")[0];
     codeText.addEventListener("click", function () {
       //commented because creating error in ps screen
@@ -239,13 +251,13 @@ function Footer(props) {
       var option = document.createElement("option");
       option.text = key;
       option.value = languageList[key];
-      option.className = extension[key]
+      option.className = extension[key];
       select.add(option);
     }
   }, []);
-  return (    
+  return (
     <motion.footer
-      style={{zIndex:"6"}}
+      style={{ zIndex: "6" }}
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.2 }}
@@ -257,23 +269,29 @@ function Footer(props) {
         </span>
         <span className="footer_text l_footer">
           <i className="fas fa-file-code"></i>{" "}
-          <input
-            className="file-name"
-            onChange={handleNameChange}
-            value={fileName}
-          />
+          <span className="file-name" onClick={ChangeFileName}>
+            {fileName + extension[editorLang]}
+          </span>
         </span>
         <span className="footer_text l_footer error" onClick={handleFileSave}>
           <i className="far fa-save"></i> Save code
         </span>
         <span className="footer_text l_footer change_lang">
           <i className="fas fa-code"></i>
-          <select name="language" id="language" onClick={ (e) => changeLangFunc(e.target.value)}></select>
+          <select
+            name="language"
+            id="language"
+            onClick={(e) => changeLangFunc(e.target.value)}
+          ></select>
         </span>
-
       </div>
       <div className="side_footer">
-      <span className="footer_text r_footer line-col-num" onClick={handleShareFile}>Share</span>
+        <span
+          className="footer_text r_footer line-col-num"
+          onClick={handleShareFile}
+        >
+          Share
+        </span>
         <span className="footer_text r_footer line-col-num">{`Ln ${Ln}, Col ${Col}`}</span>
         <span className="footer_text r_footer">Layout: US</span>
         <span className="footer_text r_footer">UTF-8</span>
